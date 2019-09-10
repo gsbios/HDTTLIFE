814508578081b457e360e8b40cea22daca42fd20
gsbios <15217636961@163.com>
2019-09-10 18:57:56 +0800

    9.10

---
+++ HDTT/HDTT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HDTT
+//
+//  Created by gsb on 2019/9/10.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HDTT/HDTT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HDTT
+//
+//  Created by gsb on 2019/9/10.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
